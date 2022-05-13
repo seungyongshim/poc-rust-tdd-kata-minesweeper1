7c00ad1220ca62dc829c09da6aea135414080741
심승용 <seungyongshim@nexon.co.kr>
2022-05-13 19:26:19 +0900

    .

--- src/mineitem.rs
+++ src/mineitem.rs
@@ -1,13 +1,27 @@
+
+pub enum Cover {
+    Covered(Cell),
+    Uncovered(Cell)
+} 
 pub enum Cell {
-    Covered(Box<Cell>),
     Bomb,
     Number(i8)
 }
 
-pub fn view(v : Cell) -> String {
-    match v {
-        Cell::Bomb => String::from("*"),
-        Cell::Number(x) => format!("{}", x),
-        Cell::Covered(_) => String::from("."),
+impl Cover {
+    pub fn to_string(&self) -> String {
+        match self {
+            Cover::Covered(_) => String::from("."),
+            Cover::Uncovered(x) => x.to_string(),
+        }
+    }
+}
+
+impl Cell {
+    pub fn to_string(&self) -> String {
+        match self {
+            Cell::Bomb => String::from("*"),
+            Cell::Number(x) => format!("{}", x),
+        }
     }
 }
--- tests/mineitem_spec.rs
+++ tests/mineitem_spec.rs
@@ -3,21 +3,21 @@ use helloworld::mineitem::{*};
 #[test]
 fn should_bomb() {
     let sut = Cell::Bomb;
-    let ret = view(sut);
+    let ret = sut.to_string();
     assert_eq!("*", ret);
 }
 
 #[test]
 fn should_numver() {
     let sut = Cell::Number(1);
-    let ret = view(sut);
+    let ret = sut.to_string();
     assert_eq!("1", ret);
 }
 
 #[test]
 fn should_cover() {
-    let sut = Cell::Covered(Box::new(Cell::Bomb));
-    let ret = view(sut);
+    let sut = Cover::Covered(Cell::Bomb);
+    let ret = sut.to_string();
     assert_eq!(".", ret);
 
 }
